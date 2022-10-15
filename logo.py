3ae6bb07d958142cb66c543dd572ad1ab61b1a10
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2022-10-15 19:19:38 +0300

    dev: moved the logo to a separate module

---
+++ logo.py
@@ -0,0 +1,21 @@
+def print_logo(logo=''):
+
+    LOGO_DEFAULT = """
+   /\                 /\\
+  / \\'._   (\_/)   _.'/ \\
+ /_.''._'--('.')--'_.''._\\
+ | \_ / `;=/ " \=;` \ _/ |
+  \/ `\__|`\___/`|__/`  \/
+   `      \(/|\)/        `
+           " ` "
+
+   QIWI_Exchange_By_VLDZ 
+"""
+
+    if logo != '':
+
+        print(logo)
+
+    else:
+
+        print(LOGO_DEFAULT)
