90639b82a1572ec8708419419304b59ec2adcda5
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2022-12-08 03:21:55 +0600

    refactoring: improved menu layout

--- menu.py
+++ menu.py
@@ -1,52 +1,35 @@
-from aiogram import types
-from dataclasses import dataclass
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+import db
 
 
-@dataclass(frozen=True)
-class data:
-    main_menu = [
-        [
-            types.KeyboardButton(text="Rate"),
-            types.KeyboardButton(text="Converter"),
-            types.KeyboardButton(text="Set currencies"),
-            types.KeyboardButton(text="About"),
-        ],
-    ]
-
-    converter_round_off = [
-        [
-            types.KeyboardButton(text="Round: off"),
-            types.KeyboardButton(text="Set currencies"),
-            types.KeyboardButton(text="Back"),
-        ],
-    ]
-
-    converter_round_on = [
-        [
-            types.KeyboardButton(text="Round: on"),
-            types.KeyboardButton(text="Set currencies"),
-            types.KeyboardButton(text="Back"),
-        ],
-    ]
+def main_menu():
+    main_menu_buttons = ReplyKeyboardBuilder().row(
+    KeyboardButton(text="Rate"),
+    KeyboardButton(text="Converter"),
+    KeyboardButton(text="Set currencies"),
+    KeyboardButton(text="About"),
+    width=2).export() 
+    
+    main_menu = ReplyKeyboardMarkup(
+        keyboard=main_menu_buttons,
+        resize_keyboard=True,
+        input_field_placeholder="Use buttons",
+    )
+    return main_menu
 
 
-main_menu = types.ReplyKeyboardMarkup(
-    keyboard=data.main_menu,
-    resize_keyboard=True,
-    width=2,
-    input_field_placeholder="Use buttons",
-)
-
-converter_round_off = types.ReplyKeyboardMarkup(
-    keyboard=data.converter_round_off,
-    resize_keyboard=True,
-    width=2,
-    input_field_placeholder="Enter value",
-)
-
-converter_round_on = types.ReplyKeyboardMarkup(
-    keyboard=data.converter_round_on,
-    resize_keyboard=True,
-    width=2,
-    input_field_placeholder="Enter value",
-)
+def converter(user_id):
+    state = "on" if db.get_round(user_id) else "off"
+    converter_buttons = ReplyKeyboardBuilder().row(
+        KeyboardButton(text=f"Round: {state}"),
+        KeyboardButton(text="Set currencies"),
+        KeyboardButton(text="Back"),
+        width=2).export() 
+        
+    converter = ReplyKeyboardMarkup(
+        keyboard=converter_buttons,
+        resize_keyboard=True,
+        input_field_placeholder="Enter value",
+    )
+    return converter
