cec34140293447e24b6de0b4c3741598f575bece
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2022-12-15 16:37:22 +0600

    dev:  made an interactive informational message and a currency change button

--- h_converter.py
+++ h_converter.py
@@ -2,11 +2,10 @@ import db
 import qiwi
 import menu
 from aiogram import F, Router
-from aiogram.methods.delete_message import DeleteMessage
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
-
+from aiogram.methods.delete_message import DeleteMessage
 router = Router()
 
 
@@ -20,22 +19,29 @@ async def converter_pressed(message: Message, state: FSMContext):
     await state.update_data(round=True)
     if db.check_user(user_id):
         if await qiwi.get_rate(user_id):
-            currency_from = db.get_from(user_id)
-            currency_to = db.get_to(user_id)
-            if currency_from != currency_to:
+            curr_from = db.get_from(user_id)
+            curr_to = db.get_to(user_id)
+            await message.answer(f"Enter how much currency you want to buy")
+            if curr_from != curr_to:
                 await message.answer(
-                    f"""
-                How much {currency_to} do you want to by for {currency_from}?""",
+                    text=f"**{curr_from}  >>  {curr_to}**",
                     parse_mode="Markdown",
                     reply_markup=menu.converter(user_id),
                 )
                 await state.set_state(Converting.converter_launched)
+                return DeleteMessage(
+                    chat_id=message.chat.id,
+                    message_id=message.message_id)
             else:
                 await message.answer(
                     """
                     Currency *from* is equal to currency *to*, set different values!""",
                     parse_mode="Markdown",
                 )
+                return DeleteMessage(
+                    chat_id=message.chat.id,
+                    message_id=message.message_id)
+
         else:
             await message.answer(
                     """
@@ -43,72 +49,70 @@ async def converter_pressed(message: Message, state: FSMContext):
                     parse_mode="Markdown",
                 )
             await message.answer("Set currencies!")
+            return DeleteMessage(
+                chat_id=message.chat.id,
+                message_id=message.message_id)
+
     else:
         await message.answer(
             """
             Send `/start` command and set currencies!""",
             parse_mode="Markdown",
         )
+        return DeleteMessage(
+            chat_id=message.chat.id,
+            message_id=message.message_id)
+
         
 @router.message(F.text == "Round: on")
-async def round_on(message: Message):
+async def round_on(message: Message) -> DeleteMessage:
     user_id = message.from_user.id
-    db.set_round(user_id, False)
+    db.set_round_state(user_id, False)
     await message.answer("Round off", reply_markup=menu.converter(user_id))
-
-
-@router.message(F.text == "Round: off")
-async def round_off(message: Message):
-    user_id = message.from_user.id
-    db.set_round(user_id, True)
-    await message.answer("Round on", reply_markup=menu.converter(user_id))
-
-@router.message(F.text == "↔️")
-async def round_off(message: Message):
-    user_id = message.from_user.id
-    curr_from = db.get_from(user_id)
-    curr_to = db.get_to(user_id)
-    db.set_from(user_id, curr_to)
-    db.set_to(user_id, curr_from)
-    currency_from = curr_to
-    currency_to = curr_from
-    await message.answer(f"""
-                From: **{currency_from}**\nTo: **{currency_to}**""",
-                parse_mode="Markdown",
-                reply_markup=menu.converter(user_id),
-                )
     return DeleteMessage(
         chat_id=message.chat.id,
         message_id=message.message_id)
 
 
+@router.message(F.text == "Round: off")
+async def round_off(message: Message) -> DeleteMessage:
+    user_id = message.from_user.id
+    db.set_round_state(user_id, True)
+    await message.answer("Round on", reply_markup=menu.converter(user_id))
+    return DeleteMessage(
+        chat_id=message.chat.id,
+        message_id=message.message_id)
+    
+
 @router.message(F.text == "Back")
 async def back(message: Message, state: FSMContext):
-    await message.answer("Back to menu", reply_markup=menu.main_menu())
+    await message.answer("Menu", reply_markup=menu.main_menu())
     await state.clear()
+    return DeleteMessage(
+        chat_id=message.chat.id,
+        message_id=message.message_id)
 
 
 @router.message(Converting.converter_launched)
-async def converter(message: Message, state: FSMContext):
+async def converter(message: Message) -> DeleteMessage:
     # user input validation
     try:
         value = float(message.text)
     except:
-        await message.answer("Enter a number, lke 10 or 10.5")
+        await message.answer("Enter a number, like 10 or 10.5")
+        return DeleteMessage(
+            chat_id=message.chat.id,
+            message_id=message.message_id)
+
     user_id = message.from_user.id
-    round = db.get_round(user_id)
-    print(round)
+    round = db.get_round_state(user_id)
     # conversion
     res = await qiwi.converter(user_id, value, round)
-    currency_from = db.get_from(user_id)
-    currency_to = db.get_to(user_id)
     await message.answer(
         f"""
-        For {value} {currency_to} you will pay `{res}` {currency_from}""",
-        parse_mode="Markdown",
+        **{value} {db.get_to(user_id)}  ==  `{res}` {db.get_from(user_id)}**""",
+        parse_mode="Markdown"
     )
-    # call menu buttons depending on rounding state
-    await message.answer(
-        "Enter another value", 
-        reply_markup = menu.converter(user_id)
-        )
+    return DeleteMessage(
+        chat_id=message.chat.id,
+        message_id=message.message_id)
--- h_set_currencies.py
+++ h_set_currencies.py
@@ -1,16 +1,24 @@
 import db
 from aiogram import F, Router
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.filters.callback_data import CallbackData
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.methods.delete_message import DeleteMessage
+from aiogram.methods.edit_message_text import EditMessageText
 from aiogram.types import Message, InlineKeyboardButton, CallbackQuery
+from dataclasses import dataclass
 
 
 router = Router()
 
-# All currencies supported by QIWI
+# all currencies supported by QIWI
 CURRENCIES = ["USD", "EUR", "RUB", "KZT", "CNY"]
 
-
+@dataclass
+class Data:
+    info_msg: Message = None
+    swap_button: InlineKeyboardButton = None
+        
+        
 class CurrencyCB(CallbackData, prefix="currency_callback"):
     user_id: int  # user id from [Telegram API](https://core.telegram.org/constructor/user)
     conv_prefix: str  # e.g. "To" or "From"
@@ -18,7 +26,7 @@ class CurrencyCB(CallbackData, prefix="currency_callback"):
 
 
 @router.message(F.text == "Set currencies")
-async def currency_pair_chooser(message: Message):
+async def currency_pair_chooser(message: Message) -> DeleteMessage:
     def generate_buttons(pref) -> InlineKeyboardBuilder:
         builder = InlineKeyboardBuilder()
         for curr in CURRENCIES:
@@ -40,17 +48,51 @@ async def currency_pair_chooser(message: Message):
 
     curr_to = generate_buttons("To")
     await message.answer("I want to buy", reply_markup=curr_to.as_markup())
+    
+    Data.swap_button = InlineKeyboardBuilder().add(InlineKeyboardButton(
+            text="↔️",
+            callback_data=CurrencyCB(
+                user_id=message.from_user.id, 
+                conv_prefix="Swap", 
+                currency=""
+            ).pack()
+        )
+    )
+
+    user_id=message.from_user.id
+    Data.info_msg = await message.answer(
+        text=f"**{db.get_from(user_id)}  >>  {db.get_to(user_id)}**",
+        parse_mode="Markdown",
+        reply_markup=Data.swap_button.as_markup())
+    
+    return DeleteMessage(
+        chat_id=message.chat.id,
+        message_id=message.message_id)
+
 
 
 @router.callback_query(CurrencyCB.filter())
-async def save_currency(callback: CallbackQuery, callback_data: CurrencyCB):
-    if callback_data.conv_prefix == "From":
-        db.set_from(callback_data.user_id, callback_data.currency)
+async def save_currency(callback: CallbackQuery, callback_data: CurrencyCB) -> DeleteMessage:
+    user_id = callback_data.user_id
+    prefix = callback_data.conv_prefix
+    currency = callback_data.currency
+    if prefix == "From":
+        db.set_from(user_id, currency)
+    elif prefix == "To":
+        db.set_to(user_id, currency)
     else:
-        db.set_to(callback_data.user_id, callback_data.currency)
-
-    await callback.message.answer(
-        f"{callback_data.conv_prefix}: **{callback_data.currency}**",
-        parse_mode="Markdown"
-    )
+        curr_from = db.get_from(user_id)
+        curr_to = db.get_to(user_id)
+        db.set_from(user_id, curr_to)
+        db.set_to(user_id, curr_from)
+        
     await callback.answer()
+    
+    edit_msg = EditMessageText(
+        chat_id=callback.message.chat.id,
+        message_id=Data.info_msg.message_id,
+        text=f"**{db.get_from(user_id)}  >>  {db.get_to(user_id)}**",
+        parse_mode="Markdown",
+        reply_markup=Data.swap_button.as_markup())
+    
+    return edit_msg
