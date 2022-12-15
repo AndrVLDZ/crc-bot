8d459deb3ffdffe01f7678fac9152311d920b6fb
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2022-12-15 16:38:54 +0600

    dev: added removing user input via DeleteMessage

--- h_rate.py
+++ h_rate.py
@@ -1,12 +1,13 @@
 import db
 import qiwi
 from aiogram import types, F, Router
+from aiogram.methods.delete_message import DeleteMessage
 
 router = Router()
 
 
 @router.message(F.text == "Rate")
-async def rate(message: types.Message):
+async def rate(message: types.Message) :
     user_id = message.from_user.id
     if db.check_user(user_id):
         rate = await qiwi.get_rate(user_id)
@@ -15,17 +16,29 @@ async def rate(message: types.Message):
             currency_to = db.get_to(user_id)
             if currency_from != currency_to:
                 await message.answer(
-                    f"For 1 {currency_to} you will pay `{rate}` {currency_from} ",
+                    f"**1 {currency_to}  ==  `{rate}` {currency_from} ",
                     parse_mode="Markdown",
                 )
+                return DeleteMessage(
+                    chat_id=message.chat.id,
+                    message_id=message.message_id)
             elif currency_from == currency_to:
                 await message.answer(
                     "Currency *from* is equal to currency *to*, set different values!",
                     parse_mode="Markdown",
                 )
+                return DeleteMessage(
+                    chat_id=message.chat.id,
+                    message_id=message.message_id)
         else:
             await message.answer("Set currencies!")
+            return DeleteMessage(
+                chat_id=message.chat.id,
+                message_id=message.message_id)
     else:
         await message.answer(
             "Send `/start` command and set currencies!", parse_mode="Markdown"
         )
+        return DeleteMessage(
+            chat_id=message.chat.id,
+            message_id=message.message_id)
