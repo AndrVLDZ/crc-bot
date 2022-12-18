e7eabc9d3a68ef4b2483ad6aaa73e2b8aa3aadea
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2022-12-18 22:28:10 +0600

    refactoring: added get_currency_pair function and improved bot answers

--- h_start.py
+++ h_start.py
@@ -6,26 +6,30 @@ import db
 
 router = Router()
 
-# /start command handler
+
 @router.message(Command(commands=["start"]))
 async def cmd_start(message: Message) -> None:
     user_id = message.from_user.id
     user_name = message.from_user.first_name
     user_surname = message.from_user.last_name
     username = message.from_user.username
+    
     if db.check_user(user_id):
-        curr_from = db.get_from(user_id)
-        curr_to = db.get_to(user_id)
+        curr_from, curr_to = db.get_currency_pair(user_id)
         # start message for old user
         await message.answer(
-            text=f"Welcome back, {user_name}!\nYour last currencies settings:\n**{curr_from}  >>  {curr_to}**""",
+            f"Welcome back, {user_name}! \
+                \nYour last currencies settings: \
+                    \nBuy:  **[{curr_to}]**    |    For:  **[{curr_from}]**",
             parse_mode="Markdown",
-            reply_markup=menu.main_menu()
+            reply_markup=menu.main_menu(),
         )
     else:
         db.add_user(user_id, user_name, user_surname, username)
         # start message for new user
-        await message.answer(f"""
-            Welcome, {user_name}!\nI can show the exchange rates of the QIWI Wallet\nAnd I also have a currency converter""",
-            reply_markup=menu.main_menu()
+        await message.answer(
+            f"Welcome, {user_name}! \
+                \nI can show the exchange rates of the QIWI Wallet \
+                    \nAnd I also have a currency converter",
+            reply_markup=menu.main_menu(),
         )
