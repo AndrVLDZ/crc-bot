93df7a22f2d25dfd13a3d3de9dbcb26544ac1e86
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2022-12-18 22:19:08 +0600

    refactoring: corrected and completed validation

--- h_converter.py
+++ h_converter.py
@@ -6,6 +6,7 @@ from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.methods.delete_message import DeleteMessage
+
 router = Router()
 
 
@@ -14,83 +15,56 @@ class Converting(StatesGroup):
 
 
 @router.message(F.text == "Converter")
-async def converter_pressed(message: Message, state: FSMContext):
+async def converter_entry_validation(message: Message, state: FSMContext):
     user_id = message.from_user.id
-    await state.update_data(round=True)
-    if db.check_user(user_id):
-        if await qiwi.get_rate(user_id):
-            curr_from = db.get_from(user_id)
-            curr_to = db.get_to(user_id)
-            await message.answer(f"Enter how much currency you want to buy")
-            if curr_from != curr_to:
-                await message.answer(
-                    text=f"**{curr_from}  >>  {curr_to}**",
-                    parse_mode="Markdown",
-                    reply_markup=menu.converter(user_id),
-                )
-                await state.set_state(Converting.converter_launched)
-                return DeleteMessage(
-                    chat_id=message.chat.id,
-                    message_id=message.message_id)
-            else:
-                await message.answer(
-                    """
-                    Currency *from* is equal to currency *to*, set different values!""",
-                    parse_mode="Markdown",
-                )
-                return DeleteMessage(
-                    chat_id=message.chat.id,
-                    message_id=message.message_id)
-
-        else:
-            await message.answer(
-                    """
-                    Currency *from* is equal to currency *to*, set different values!""",
-                    parse_mode="Markdown",
-                )
-            await message.answer("Set currencies!")
-            return DeleteMessage(
-                chat_id=message.chat.id,
-                message_id=message.message_id)
-
-    else:
+    if not db.check_user(user_id):
         await message.answer(
-            """
-            Send `/start` command and set currencies!""",
+            "Send `/start` command first!",
+            parse_mode="Markdown"
+            )
+    else:
+        curr_from, curr_to = db.get_currency_pair(user_id)
+        await message.answer(
+            f"Enter how much currency you want to buy \
+                \nBuy:  **[{curr_to}]**    |    For:  **[{curr_from}]**",
             parse_mode="Markdown",
+            reply_markup=menu.converter(user_id)
         )
-        return DeleteMessage(
-            chat_id=message.chat.id,
-            message_id=message.message_id)
+        await state.set_state(Converting.converter_launched)
 
-        
+
 @router.message(F.text == "Round: on")
 async def round_on(message: Message) -> DeleteMessage:
     user_id = message.from_user.id
     db.set_round_state(user_id, False)
-    await message.answer("Round off", reply_markup=menu.converter(user_id))
+    await message.answer(
+        "Round off",
+        reply_markup=menu.converter(user_id)
+        )
     return DeleteMessage(
         chat_id=message.chat.id,
-        message_id=message.message_id)
+        message_id=message.message_id
+    )
 
 
 @router.message(F.text == "Round: off")
 async def round_off(message: Message) -> DeleteMessage:
     user_id = message.from_user.id
     db.set_round_state(user_id, True)
-    await message.answer("Round on", reply_markup=menu.converter(user_id))
+    await message.answer(
+        "Round on",
+        reply_markup=menu.converter(user_id)
+        )
     return DeleteMessage(
         chat_id=message.chat.id,
-        message_id=message.message_id)
-    
+        message_id=message.message_id
+    )
+
 
 @router.message(F.text == "Back")
 async def back(message: Message, state: FSMContext):
     await message.answer("Menu", reply_markup=menu.main_menu())
     await state.clear()
-    return DeleteMessage(
-        chat_id=message.chat.id,
-        message_id=message.message_id)
 
 
 @router.message(Converting.converter_launched)
@@ -100,19 +74,28 @@ async def converter(message: Message) -> DeleteMessage:
         value = float(message.text)
     except:
         await message.answer("Enter a number, like 10 or 10.5")
-        return DeleteMessage(
-            chat_id=message.chat.id,
-            message_id=message.message_id)
-
+        
+    # getting user settings
     user_id = message.from_user.id
     round = db.get_round_state(user_id)
-    # conversion
+    
+    # calling and validating the converter function
     res = await qiwi.converter(user_id, value, round)
-    await message.answer(
-        f"""
-        **{value} {db.get_to(user_id)}  ==  `{res}` {db.get_from(user_id)}**""",
-        parse_mode="Markdown"
-    )
+    if not res:
+        await message.answer(
+            "Set different currencies!!",
+            parse_mode="Markdown"
+        )
+    else:
+        curr_from, curr_to = db.get_currency_pair(user_id)
+        if curr_from != curr_to and curr_from in qiwi.CODES and curr_to in qiwi.CODES: 
+            await message.answer(
+                f"**{value} {curr_to}  ==  `{res}` {curr_from}**",
+                parse_mode="Markdown"
+            )
+
+    # removing user input for better readability of converter responses
     return DeleteMessage(
         chat_id=message.chat.id,
-        message_id=message.message_id)
+        message_id=message.message_id
+    )
