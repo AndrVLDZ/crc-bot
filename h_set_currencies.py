23e53f05f252d2d4d770e441064d1f759902a9cf
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2022-12-19 17:36:19 +0600

    dev: added validation for set currencies menu button

--- h_set_currencies.py
+++ h_set_currencies.py
@@ -21,49 +21,55 @@ class CurrencyCB(CallbackData, prefix="currency_callback"):
 @router.message(F.text == "Set currencies")
 async def currency_pair_chooser(message: Message, state: FSMContext):
     user_id=message.from_user.id
-    def generate_buttons(pref) -> InlineKeyboardBuilder:
-        builder = InlineKeyboardBuilder()
-        for curr in CURRENCIES:
-            cb_data = CurrencyCB(
-                user_id=user_id,
-                conv_prefix=pref,
-                currency=curr,
-            )
-            builder.add(
-                InlineKeyboardButton(
-                    text=curr, 
-                    callback_data=cb_data.pack()
-                    )
-            )
-        return builder
-
-    curr_from = generate_buttons("From")
-    await message.answer("I have", reply_markup=curr_from.as_markup())
-
-    curr_to = generate_buttons("To")
-    await message.answer("I want to buy", reply_markup=curr_to.as_markup())
-    
-    swap_button = InlineKeyboardBuilder().add(
-        InlineKeyboardButton(
-            text="↔️",
-            callback_data=CurrencyCB(
-                user_id=user_id,
-                conv_prefix="Swap",
-                currency="",
-            ).pack(),
+    if not await db.check_user(user_id):
+        await message.answer(
+            "Send `/start` command first!",
+            parse_mode="Markdown"
         )
-    )
-    curr_from, curr_to = await db.get_currency_pair(user_id)
-    info_msg = await message.answer(
-        text=f"Buy:  **[{curr_to}]**    |    For:  **[{curr_from}]**",
-        parse_mode="Markdown",
-        reply_markup=swap_button.as_markup())
-    
-    user_data = {
-        "message_id": info_msg.message_id,
-        "swap_button": swap_button,
-    }
-    await state.set_data(user_data)
+    else:
+        def generate_buttons(pref) -> InlineKeyboardBuilder:
+            builder = InlineKeyboardBuilder()
+            for curr in CURRENCIES:
+                cb_data = CurrencyCB(
+                    user_id=user_id,
+                    conv_prefix=pref,
+                    currency=curr,
+                )
+                builder.add(
+                    InlineKeyboardButton(
+                        text=curr, 
+                        callback_data=cb_data.pack()
+                        )
+                )
+            return builder
+
+        curr_from = generate_buttons("From")
+        await message.answer("I have", reply_markup=curr_from.as_markup())
+
+        curr_to = generate_buttons("To")
+        await message.answer("I want to buy", reply_markup=curr_to.as_markup())
+        
+        swap_button = InlineKeyboardBuilder().add(
+            InlineKeyboardButton(
+                text="↔️",
+                callback_data=CurrencyCB(
+                    user_id=user_id,
+                    conv_prefix="Swap",
+                    currency="",
+                ).pack(),
+            )
+        )
+        curr_from, curr_to = await db.get_currency_pair(user_id)
+        info_msg = await message.answer(
+            text=f"Buy:  **[{curr_to}]**    |    For:  **[{curr_from}]**",
+            parse_mode="Markdown",
+            reply_markup=swap_button.as_markup())
+        
+        user_data = {
+            "message_id": info_msg.message_id,
+            "swap_button": swap_button,
+        }
+        await state.set_data(user_data)
 
 
 @router.callback_query(CurrencyCB.filter())
