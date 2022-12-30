987a34085bb9b9705c546f3f8e057c623204372b
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2022-12-30 14:51:05 +0600

    refactoring: user check moved to tools.py and unnecessary handler removed

--- h_about.py
+++ h_about.py
@@ -1,7 +1,7 @@
+from tools import check_user
 from aiogram import F, Router, __version__
-from aiogram.filters.text import Text
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from aiogram.types import InlineKeyboardButton, CallbackQuery, Message
+from aiogram.types import InlineKeyboardButton, Message
 
 router = Router()
 
@@ -16,23 +16,19 @@ Powered by aiogram {__version__}
 
 @router.message(F.text == "About")
 async def about(message: Message):
-    builder = InlineKeyboardBuilder()
-    builder.row(InlineKeyboardButton(
-        text="Telegram", 
-        url="https://t.me/andrvldz")
-    )
-    builder.row(InlineKeyboardButton(
-        text="GitHub", 
-        url="https://github.com/AndrVLDZ")
-    )
-    await message.answer(
-        msg_about,
-        parse_mode="Markdown",
-        reply_markup=builder.as_markup(),
-    )
-
-
-@router.callback_query(Text(text="guide"))
-async def guide(callback: CallbackQuery):
-    await callback.message.answer("Guide for user [in develop]")
-    await callback.answer()
+    user_id = message.from_user.id
+    if await check_user(user_id):
+        builder = InlineKeyboardBuilder()
+        builder.row(InlineKeyboardButton(
+            text="Telegram", 
+            url="https://t.me/andrvldz")
+        )
+        builder.row(InlineKeyboardButton(
+            text="GitHub", 
+            url="https://github.com/AndrVLDZ")
+        )
+        await message.answer(
+            msg_about,
+            parse_mode="Markdown",
+            reply_markup=builder.as_markup(),
+        )
