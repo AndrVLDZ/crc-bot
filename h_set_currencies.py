db4a00b92ba93e517264f2ca385e62755a98d9c3
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2022-12-30 14:08:43 +0600

    refactoring: separated two new functions from currency_pair_chooser

--- h_set_currencies.py
+++ h_set_currencies.py
@@ -13,13 +13,58 @@ CURRENCIES = ["USD", "EUR", "RUB", "KZT", "CNY"]
 
 
 class CurrencyCB(CallbackData, prefix="currency_callback"):
-    user_id: int  
-    conv_prefix: str  # e.g. "To" or "From"
+    user_id: int  # telegram user ID
+    conv_prefix: str  # e.g. "To" or "From" 
     currency: str  # e.g. "USD", "RUB", ...
 
 
+def generate_buttons(user_id: int, pref: str) -> InlineKeyboardBuilder:
+    builder = InlineKeyboardBuilder()
+    for curr in CURRENCIES:
+        cb_data = CurrencyCB(
+            user_id=user_id,
+            conv_prefix=pref,
+            currency=curr,
+        )
+        builder.add(
+            InlineKeyboardButton(
+                text=curr, 
+                callback_data=cb_data.pack()
+                )
+        )
+    return builder
+
+
+async def generate_info_msg(user_id: int, message: Message, state: FSMContext):
+    swap_button = InlineKeyboardBuilder().add(
+        InlineKeyboardButton(
+            text="↔️",
+            callback_data=CurrencyCB(
+                user_id=user_id,
+                conv_prefix="Swap",
+                currency="",
+            ).pack(),
+        )
+    )
+    
+    curr_from, curr_to = await db.get_currency_pair(user_id)
+    info_msg = await message.answer(
+        text=f"Buy:  **[{curr_to}]**    |    For:  **[{curr_from}]**",
+        parse_mode="Markdown",
+        reply_markup=swap_button.as_markup()
+    )
+    
+    user_data = {
+        "chat_id": info_msg.chat.id,
+        "message_id": info_msg.message_id,
+        "swap_button": swap_button,
+    }
+    
+    await state.set_data(user_data)
+
+
 @router.message(F.text == "Set currencies")
-async def currency_pair_chooser(message: Message, state: FSMContext):
+async def currency_pair_chooser(message: Message):
     user_id=message.from_user.id
     if not await db.check_user_id(user_id):
         await message.answer(
@@ -27,51 +72,14 @@ async def currency_pair_chooser(message: Message, state: FSMContext):
             parse_mode="Markdown"
         )
     else:
-        def generate_buttons(pref) -> InlineKeyboardBuilder:
-            builder = InlineKeyboardBuilder()
-            for curr in CURRENCIES:
-                cb_data = CurrencyCB(
-                    user_id=user_id,
-                    conv_prefix=pref,
-                    currency=curr,
-                )
-                builder.add(
-                    InlineKeyboardButton(
-                        text=curr, 
-                        callback_data=cb_data.pack()
-                        )
-                )
-            return builder
-
-        curr_from = generate_buttons("From")
+        curr_from = generate_buttons(user_id, "From")
         await message.answer("I have", reply_markup=curr_from.as_markup())
 
-        curr_to = generate_buttons("To")
+        curr_to = generate_buttons(user_id, "To")
         await message.answer("I want to buy", reply_markup=curr_to.as_markup())
         
-        swap_button = InlineKeyboardBuilder().add(
-            InlineKeyboardButton(
-                text="↔️",
-                callback_data=CurrencyCB(
-                    user_id=user_id,
-                    conv_prefix="Swap",
-                    currency="",
-                ).pack(),
-            )
-        )
-        curr_from, curr_to = await db.get_currency_pair(user_id)
-        info_msg = await message.answer(
-            text=f"Buy:  **[{curr_to}]**    |    For:  **[{curr_from}]**",
-            parse_mode="Markdown",
-            reply_markup=swap_button.as_markup())
+        generate_info_msg(user_id, message)
         
-        user_data = {
-            "chat_id": info_msg.chat.id,
-            "message_id": info_msg.message_id,
-            "swap_button": swap_button,
-        }
-        await state.set_data(user_data)
-
 
 @router.callback_query(CurrencyCB.filter())
 async def save_currency(callback: CallbackQuery, callback_data: CurrencyCB, state: FSMContext):
@@ -79,6 +87,7 @@ async def save_currency(callback: CallbackQuery, callback_data: CurrencyCB, stat
     user_id = callback_data.user_id
     prefix = callback_data.conv_prefix
     currency = callback_data.currency
+    
     if prefix == "From":
         await db.set_from(user_id, currency)
     if prefix == "To":
@@ -91,6 +100,7 @@ async def save_currency(callback: CallbackQuery, callback_data: CurrencyCB, stat
     
     user_data = await state.get_data()
     curr_from, curr_to = await db.get_currency_pair(user_id)
+    
     return EditMessageText(
         chat_id=user_data["chat_id"],
         message_id=user_data["message_id"],
