64310904cc8f98d5b18a8a87db947ca3adabbfe3
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2023-01-05 16:26:17 +0600

    refactoring: removed unused database fields and added message for new user

--- h_start.py
+++ h_start.py
@@ -6,13 +6,24 @@ from aiogram.filters import Command
 
 router = Router()
 
+# the bot will automatically calculate the value of the expression and translate the result into the final currency.
+
+new_user_msg: str = f""" 
+I have currency converter, calculator and monitoring of QIWI Wallet exchange rates.
+The converter is always active after the start, just send a number or a mathematical expression.
+\nMenu buttons & commands:
+[Rate] — returns the exchange rate of the currency pair selected by [set currencies]
+[Set currencies] — here you can specify your currency and the currency you want to buy
+[Round] — allows you to enable or disable rounding for the [Rate] and converter
+/about — information about the program and the author, links and ways to contact the developer
+/help — how to use the bot
+\nIn addition, rates are updated every minute and in decimal fractions you can put a comma instead of a point, the bot will understand everything.
+"""
 
 @router.message(Command(commands=["start"]))
 async def cmd_start(message: Message) -> None:
     user_id = message.from_user.id
     user_name = message.from_user.first_name
-    user_surname = message.from_user.last_name
-    username = message.from_user.username
     
     if await db.check_user_id(user_id):
         main_menu = await menu.main_menu(user_id)
@@ -27,11 +38,20 @@ async def cmd_start(message: Message) -> None:
         )
     else:
         main_menu = await menu.main_menu(user_id, new_user=True)
-        await db.add_user(user_id, user_name, user_surname, username)
+        await db.add_user(user_id)
         # start message for new user
         await message.answer(
-            f"Welcome, {user_name}! \
-                \nI can show the exchange rates of the QIWI Wallet \
-                    \nAnd I also have a currency converter",
+            f"Welcome, {user_name}!\n{new_user_msg}",
             reply_markup=main_menu,
         )
+
+#  QIWI exchange rates are updated every minute
+#  The converter is always active after the start, just send a number or a mathematical expression, the bot will automatically calculate the value of the expression and translate the result into the final currency
+#  In addition, in decimal fractions, you can put a comma instead of a point, the bot will understand everything
+# 
+#  [Rate] — returns the exchange rate of the currency pair selected by [set currencies]
+#  [Round] — allows you to enable or disable rounding for the [Rate] and converter
+#  [Set currencies] — here you can specify your currency and the currency you want to buy
+#  Your settings will be displayed under the currency setting buttons in the information message and there is also a currency swap button
+#  /about — information about the program and the author, links and ways to contact the developer
+#  /help — how to use the bot | If you do not understand how to use - send /help
