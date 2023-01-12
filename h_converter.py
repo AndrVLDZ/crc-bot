20d2bd386c09ed958d38e9ea8b16ec375d5b472a
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2023-01-12 23:00:58 +0600

    refactoring: split the convert function into 3 separate and added convert function from qiwi.py

--- h_converter.py
+++ h_converter.py
@@ -3,6 +3,7 @@ import qiwi
 import menu
 from math import isnan, isinf
 from tools import check_user
+from typing import Union
 from aiogram import Router
 from aiogram.types import Message
 from cexprtk import evaluate_expression as evaluate
@@ -10,74 +11,76 @@ from cexprtk import evaluate_expression as evaluate
 router = Router()
 
 
+async def evaluate_value(message: Message) -> float:
+    try:
+        res = evaluate(
+            message.text.replace(",",".").replace(" ", ""), {}
+            )
+        return float(res)
+    except:
+        main_menu = await menu.main_menu(message.from_user.id)
+        await message.answer(
+            text="Enter a number or a valid math expression",
+            reply_markup=main_menu,
+        )
+
+
+async def validate_evaluated_value(message: Message, value: float) -> bool:
+    user_id = message.from_user.id
+    main_menu = await menu.main_menu(user_id)
+    if isnan(value):
+        await message.answer(
+            text="Result is undefined",
+            reply_markup=main_menu,
+        )
+        return False
+    
+    if isinf(value):
+        await message.answer(
+            text="Cannot divide by zero",
+            reply_markup=main_menu,
+        )
+        return False
+    
+    return True
+
+
+async def convert_value(message: Message, value: float, round_res: bool) -> Union[str, float]:
+    rate = await qiwi.get_rate(message.from_user.id, converter=True)
+    if not rate:
+        return "Set different currencies!"
+    
+    res = value*rate
+    if round_res:
+        rounded_res = round(res, 2)
+        if res != 0 and rounded_res == 0:
+            return "Zero value received after rounding"
+        return rounded_res
+    
+    return res
+
+
 @router.message()
-async def converter(message: Message):
+async def converting(message: Message):
     if await check_user(message):
         user_id = message.from_user.id
-        main_menu = await menu.main_menu(user_id)
-        # variable to store user input
-        value: float = .0
-        # trying to evaluate user input
-        try:
-            value = evaluate(
-                message.text.replace(",",".").replace(" ", ""), {}
-                )
-            print(value, type(value))
-        except:
-            await message.answer(
-                text="Enter a number or a valid math expression",
-                reply_markup=main_menu,
-            )
-            return
-           
-        # getting user settings
-        curr_from, curr_to = await db.get_currency_pair(user_id)
-        # calling and validating the converter function
-        if curr_from == curr_to:
-            await message.answer(
-                text="Set different currencies!",
-                reply_markup=main_menu,
-            )
-            return
-        
-        # getting user settings
+        value = await evaluate_value(message)
+        if not await validate_evaluated_value(message, value):
+            return            
+    
         round_res = await db.get_round_state(user_id)
-        
-        res = await qiwi.converter(user_id, value, round_res)
-        if type(res) == float and res == 0 and round_res:
+        main_menu = await menu.main_menu(user_id) 
+        res = await convert_value(message, value, round_res)
+        if type(res) == str:
             await message.answer(
-                text="Zero result after rounding",
+                text=res,
                 reply_markup=main_menu,
             )
             return
         
-        if type(res) == float and res == 0 and not round_res:
-            await message.answer(
-                text="Zero result",
-                reply_markup=main_menu,
-            )
-            return
-        
-        if isnan(res):
-            await message.answer(
-                text="Result is undefined",
-                reply_markup=main_menu,
-            )
-            return
-        
-        if isinf(res):
-            await message.answer(
-                text="Cannot divide by zero",
-                reply_markup=main_menu,
-            )
-            return
-            
-        if res: 
-            if round_res: 
-                value = round(value, 4)
-            await message.answer(
-                f"**`{value}` {curr_to}  ==  `{res}` {curr_from}**",
-                parse_mode="Markdown",
-                reply_markup=main_menu,
-            )
-
+        curr_from, curr_to = await db.get_currency_pair(user_id)
+        await message.answer(
+            f"**`{value}` {curr_to}  ==  `{res}` {curr_from}**",
+            parse_mode="Markdown",
+            reply_markup=main_menu,
+        )
