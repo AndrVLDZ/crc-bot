a0353fc809543f346d7b0c5aa2a5747f51ff178b
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2023-01-20 17:20:22 +0600

    ref: many typing improvements and converter_answer function separated from converting

--- h_converter.py
+++ h_converter.py
@@ -11,23 +11,26 @@ from cexprtk import evaluate_expression as evaluate
 router = Router()
 
 
-async def evaluate_value(message: Message) -> float:
-    try:
-        res = evaluate(
-            message.text.replace(",",".").replace(" ", ""), {}
+async def evaluate_value(user_id: int, message: Message) -> Union[float, bool]:
+    if isinstance(message.text, str):
+        try:
+            res = evaluate(
+                message.text.replace(",",".").replace(" ", ""), {}
+                )
+            return float(res)
+        except:
+            menu = await main_menu(user_id)
+            await message.answer(
+                text="Enter a number or a valid math expression",
+                reply_markup=menu,
             )
-        return float(res)
-    except:
-        menu = await main_menu(message.from_user.id)
-        await message.answer(
-            text="Enter a number or a valid math expression",
-            reply_markup=menu,
-        )
+            return False
+    return False
 
 
-async def validate_evaluated_value(message: Message, value: float) -> bool:
+async def validate_evaluated_value(user_id: int, message: Message, value: float) -> bool:
     if isnan(value):
-        menu = await main_menu(message.from_user.id)
+        menu = await main_menu(user_id)
         await message.answer(
             text="Result is undefined",
             reply_markup=menu,
@@ -35,57 +38,60 @@ async def validate_evaluated_value(message: Message, value: float) -> bool:
         return False
     
     if isinf(value):
-        menu = await main_menu(message.from_user.id)
+        menu = await main_menu(user_id)
         await message.answer(
             text="Cannot divide by zero",
             reply_markup=menu,
         )
         return False
-    
     return True
 
 
-async def convert_value(message: Message, value: float, round_res: bool) -> Union[str, float]:
-    rate = await get_rate(message.from_user.id, converter=True)
+async def convert_value(user_id: int, value: float, round_res: bool, message: Message) -> Union[bool, float]:
+    rate = await get_rate(user_id, converter=True)
     if not rate:
-        return "Set different currencies!"
+        menu = await main_menu(user_id) 
+        await message.answer(
+            text="Set different currencies!",
+            reply_markup=menu,
+        )
+        return False
     
-    res = value*rate
-    
+    res: float = value*rate
     if round_res:
         rounded_res = round(res, 2)
         if res != 0 and rounded_res == 0:
-            return "Zero value received after rounding"
+            menu = await main_menu(user_id)
+            await message.answer(
+                text="Zero value received after rounding",
+                reply_markup=menu,
+            )
+            return False
         return rounded_res
-    
     return res
+
+
+async def converter_answer(user_id: int, value: float, res: float, message: Message):
+    curr_from, curr_to = await get_currency_pair(user_id)
+    menu = await main_menu(user_id) 
+    await message.answer(
+        f"**`{value}` {curr_to}  ==  `{res}` {curr_from}**",
+        parse_mode="Markdown",
+        reply_markup=menu,
+    )
 
 
 @router.message()
 async def converting(message: Message):
-    if await check_user(message):
-        user_id = message.from_user.id
-        value = await evaluate_value(message)
-        if not await validate_evaluated_value(message, value):
-            return            
-    
-        menu = await main_menu(user_id) 
-        round_res = await get_round_state(user_id)
-        
-        res = await convert_value(message, value, round_res)
-        if type(res) == str:
-            await message.answer(
-                text=res,
-                reply_markup=menu,
-            )
-            return
-        
-        if round_res:
-            value = round(value, 2)
-        
-        curr_from, curr_to = await get_currency_pair(user_id)
-        await message.answer(
-            f"**`{value}` {curr_to}  ==  `{res}` {curr_from}**",
-            parse_mode="Markdown",
-            reply_markup=menu,
-        )
+    user_id = await check_user(message)
+    if user_id:
+        value = await evaluate_value(user_id, message)
+        if value is not False: 
+            if not await validate_evaluated_value(user_id, message, value):
+                return            
+            round_res = await get_round_state(user_id)
+            res = await convert_value(user_id, value, round_res, message)
+            if res is not False:        
+                if round_res:
+                    value = round(value, 2)
+                await converter_answer(user_id, value, res, message)
