a672c467230956c3a5b183cc8bbb9fcb27af5af1
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2023-06-17 10:02:02 +0600

    dev: renamed bot.py, switched to a self-written async scheduler instead of periodic

---
+++ main.py
@@ -0,0 +1,62 @@
+from asyncio import get_event_loop, run, sleep
+from platform import python_version
+
+from aiogram import Bot, Dispatcher
+
+import h_about
+import h_converter
+import h_help
+import h_rate
+import h_round
+import h_set_currencies
+import h_start
+from db import create_table
+from get_tokens import get_secrets
+from logs import log
+from qiwi import get_rates
+
+bot_token, qiwi_token = get_secrets()
+
+bot = Bot(token=bot_token)
+dp = Dispatcher()
+
+# connecting handlers
+dp.include_router(h_start.router)
+dp.include_router(h_help.router)
+dp.include_router(h_about.router)
+dp.include_router(h_set_currencies.router)
+dp.include_router(h_rate.router)
+dp.include_router(h_round.router)
+dp.include_router(h_converter.router)
+
+
+# exchange rates update
+async def upd_rates():
+    await get_rates(qiwi_token)
+    # log.info(f"RATES UPDATED")
+
+
+# periodic tasks for event loop
+async def scheduler():
+    while True:
+        try:
+            await upd_rates()
+        except Exception as e:
+            log.error(f"Failed to update rates: {e}")
+        await sleep(60)  # exchange rates update every minute
+
+
+async def main() -> None:
+    log.info(f"Python version: {python_version()}")
+    # create table if not exists
+    await create_table()
+    # getting exchange rates
+    await get_rates(qiwi_token)
+    loop = get_event_loop()
+    loop.create_task(scheduler())
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, loop=loop) 
+
+
+if __name__ == "__main__":
+    run(main())
