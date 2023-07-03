9fb6d0f05c7ba832f409623958b43799d8c5c5c0
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2023-07-03 16:19:46 +0600

    def: added user_data save/load and refactoring

--- db.py
+++ db.py
@@ -1,173 +1,159 @@
 import aiosqlite
+import json
 
 
-db_name: str = "bot.sqlite"
+DB: str = "db.sqlite"
 
+
+async def execute_query(query: str, params: tuple = ()) -> None:
+    async with aiosqlite.connect(DB) as db:
+        async with db.cursor() as cursor:
+            await cursor.execute(query, params)
+            await db.commit()
+
+
+async def fetch_query(query: str, params: tuple = ()) -> tuple:
+    async with aiosqlite.connect(DB) as db:
+        async with db.execute(query, params) as cursor:
+            result = await cursor.fetchone()
+            return result
+        
 
 async def create_table() -> None:
-    async with aiosqlite.connect(db_name) as db:
-        async with db.cursor() as c:
-            await c.execute(
-                """
-                    CREATE TABLE IF NOT EXISTS users 
-                    (id       INT   PRIMARY KEY, 
-                    curr_from TEXT, 
-                    curr_to   TEXT,
-                    round     BOOL);
-                    """
-            )
-        await db.commit()
+    query = """
+                CREATE TABLE IF NOT EXISTS users 
+                (id       INT   PRIMARY KEY, 
+                curr_from TEXT, 
+                curr_to   TEXT,
+                round     BOOL,
+                user_data TEXT);
+            """
+    await execute_query(query)
 
 
 async def add_user(id: int) -> None:
     # default user configuration
-    curr_from: str = "USD"
-    curr_to: str = "EUR"
+    curr_from: str = "USD 🇺🇸"
+    curr_to: str = "EUR 🇪🇺"
     round: bool = True
+    user_data: str = ""
     # rows to be added to the database
-    rows = ("id", "curr_from", "curr_to", "round")
-    async with aiosqlite.connect(db_name) as db:
-        async with db.cursor() as c:
-            await c.execute(
-                f"""
+    rows = ("id", "curr_from", "curr_to", "round", "user_data")
+    query = f"""
                 INSERT OR REPLACE INTO users {rows} 
-                VALUES (?, ?, ?, ?)
-                """,
-                (id, curr_from, curr_to, round,),
-            )
-        await db.commit()
+                VALUES (?, ?, ?, ?, ?)
+            """
+    await execute_query(query, (id, curr_from, curr_to, round, user_data))
 
 
 async def user_in_db(user_id: int) -> bool:
-    async with aiosqlite.connect(db_name) as db:
-        async with db.execute(
-            f"""
-            SELECT *
-            FROM users 
-            WHERE id = ?
-            """,
-            (user_id,),
-        ) as c:
-            user_row = await c.fetchone()
-            return bool(user_row)  # None = False, tuple = True
+    query = f"""
+                SELECT *
+                FROM users 
+                WHERE id = ?
+            """
+    user = await fetch_query(query, (user_id,))
+    return bool(user)    
 
 
 async def set_from(user_id: int, currency_from: str) -> None:
-    async with aiosqlite.connect(db_name) as db:
-        async with db.cursor() as c:
-            await c.execute(
-                f"""
+    query =f"""
                 UPDATE users 
                 SET curr_from = ?
                 WHERE id = ?
-                """,
-                (currency_from, user_id,),
-            )
-        await db.commit()
+            """
+    await execute_query(query, (currency_from, user_id,))
+
 
 
 async def set_to(user_id: int, currency_to: str) -> None:
-    async with aiosqlite.connect(db_name) as db:
-        async with db.cursor() as c:
-            await c.execute(
-                f"""
+    query = f"""
                 UPDATE users 
                 SET curr_to = ?
                 WHERE id = ?
-                """,
-                (currency_to, user_id,),
-            )
-        await db.commit()
+            """
+    await execute_query(query, (currency_to, user_id,))
 
 
-async def set_currency_pair(user_id: int, curr_from, curr_to: str) -> None:
-    async with aiosqlite.connect(db_name) as db:
-        async with db.cursor() as c:
-            await c.execute(
-                f"""
+
+async def set_currency_pair(user_id: int, curr_from: str, curr_to: str) -> None:
+    query = """
                 UPDATE users 
                 SET curr_from = ?,
                     curr_to = ?
                 WHERE id = ?
-                """,
-                (curr_from, curr_to, user_id,),
-            )
-        await db.commit()
+            """
+    await execute_query(query, (curr_from, curr_to, user_id,))
+
 
 
 async def set_round_state(user_id: int, round: bool) -> None:
-    async with aiosqlite.connect(db_name) as db:
-        async with db.cursor() as c:
-                await c.execute(
-                f"""
+    query = """
                 UPDATE users 
                 SET round = ?
                 WHERE id = ?
-                """,
-                (round, user_id,),
-            )
-        await db.commit()
+            """
+    await execute_query(query, (round, user_id,))
+
 
 
 async def get_from(user_id: int) -> str:
-    async with aiosqlite.connect(db_name) as db:
-        async with db.execute(
-            f"""
-            SELECT curr_from
-            FROM users 
-            WHERE id = ?
-            """,
-            (user_id,),
-        ) as c:
-            result = await c.fetchone()
-            if result is None:
-                raise IndexError("User does not exist")
-            return result[0]
+    query = """
+                SELECT curr_from
+                FROM users 
+                WHERE id = ?
+            """
+    result = await fetch_query(query, (user_id,))
+    return result[0]
 
 
 async def get_to(user_id: int) -> str:
-    async with aiosqlite.connect(db_name) as db:
-        async with db.execute(
-            f"""
-            SELECT curr_to
-            FROM users 
-            WHERE id = ?
-            """,
-            (user_id,),
-        ) as c:
-            result = await c.fetchone()
-            if result is None:
-                raise IndexError("User does not exist")
-            return result[0]
+    query = """
+                SELECT curr_to
+                FROM users 
+                WHERE id = ?
+            """
+    result = await fetch_query(query, (user_id,))
+    return result[0]
 
 
 async def get_currency_pair(user_id: int) -> tuple[str, str]:
-    async with aiosqlite.connect(db_name) as db:
-        async with db.execute(
-            f"""
-            SELECT curr_from, curr_to
-            FROM users 
-            WHERE id = ?
-            """,
-            (user_id,),
-        ) as c:
-            result = await c.fetchone()
-            if result is None:
-                raise IndexError("User does not exist")
-            return result[0], result[1]
+    query = """
+                SELECT curr_from, curr_to
+                FROM users 
+                WHERE id = ?
+            """
+    result = await fetch_query(query, (user_id,))
+    return result[0], result[1]
 
 
 async def get_round_state(user_id: int) -> bool:
-    async with aiosqlite.connect(db_name) as db:
-        async with db.execute(
-            f"""
-            SELECT round
-            FROM users 
-            WHERE id = ?
-            """,
-            (user_id,),
-        ) as c:
-            result = await c.fetchone()
-            if result is None:
-                raise IndexError("User does not exist")
-            return bool(result[0])
+    query = """
+                SELECT round
+                FROM users 
+                WHERE id = ?
+            """
+    result = await fetch_query(query, (user_id,))
+    return bool(result[0])
+
+
+async def save_user_data(user_id: int, user_data: dict) -> None:
+    user_data_json = json.dumps(user_data)
+    query = """
+                UPDATE users 
+                SET user_data = ?
+                WHERE id = ?
+            """
+    await execute_query(query, (user_data_json, user_id,))
+
+
+async def load_user_data(user_id: int) -> dict:
+    query = """
+                SELECT user_data
+                FROM users 
+                WHERE id = ?
+            """
+    user_data_db = await fetch_query(query, (user_id,))
+    # print(user_data_json)
+    user_data = json.loads(user_data_db[0])
+    return user_data
